660eccfc90e0ba2cacd0ae698d990ad2da3792ae
Harshit.Nigam <Harshit.Nigam@LP-HarshitNigam.igglobal.com>
2019-07-10 16:15:27 +0530

    First commit..

---
+++ src/any/any.java
@@ -0,0 +1,17 @@
+package any;
+
+public class any {
+
+	int a= 20; 
+	static int b =30;
+	public static void main (String args[])
+	{
+		int c = 40;
+		System.out.println(b);
+		any ab = new any();
+		System.out.println(ab.a);
+		System.out.println(ab.b);
+		System.out.println(c);
+	//	System.out.println(a+" "+" "+c);
+	}
+}
