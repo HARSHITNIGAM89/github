7075832038ec1873a5717c7a1e5fc070eaec7e95
harshit <plusmobileinfogain@gmail.com>
2019-07-11 14:52:53 +0530

    commit by lead

---
+++ src/any/test2.java
@@ -0,0 +1,19 @@
+package any;
+
+public class test2 {
+
+	int a= 20; 
+	static int b =30;
+	static int f =30;
+	public static void main (String args[])
+	{
+		int c = 40;
+		System.out.println(b);
+		test2 ab = new test2();
+		System.out.println(ab.a);
+		System.out.println(ab.b);
+		System.out.println(c);
+		System.out.println(f);
+	//	System.out.println(a+" "+" "+c);
+	}
+}
